fc797560cf6204c97347c2a7fb24580f010ef9f8
Saifullah Ansari <saifullah.ansari@gmail.com>
2024-01-15 18:10:17 +0100

    First commit

---
+++ src/main/java/com/example/montyhall/montyhallpuzzle/MontyHallPuzzleApplication.java
@@ -0,0 +1,13 @@
+package com.example.montyhall.montyhallpuzzle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MontyHallPuzzleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MontyHallPuzzleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/montyhall/montyhallpuzzle/MontyHallPuzzleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.montyhall.montyhallpuzzle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MontyHallPuzzleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
